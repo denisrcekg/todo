97601c5559dcf70630d75287e4b61d56ddcaff73
denisrcekg <36004846+denisrcekg@users.noreply.github.com>
2021-01-22 01:06:03 +0600

    added text, created_at, is_closed, is favorite params for todo list items in models.py. Registered Todo project. Added changed.htm, deleted.htm, success.htm files into templates folder(as in HW tasks)

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    text = models.CharField(max_length=100) #mal lenght 100 symbols
+    created_at = models.DateField(auto_now_add=True) # auto-insert date-time
+    is_closed = models.BooleanField(default=False) # task is done = False
+    is_favorite = models.BooleanField(default=False) # Task is not favorite by default
+    
+
