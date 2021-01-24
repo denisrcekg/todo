0245a05efd1e15352bb2cabb1535e52b4b0d810c
denisrcekg <36004846+denisrcekg@users.noreply.github.com>
2021-01-24 23:27:25 +0600

    this version is works. In urls.py added url paths: add-todo, del, mark, close (Atay helped to solve)

--- todo/urls.py
+++ todo/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import homepage, todo, second, third, bookstore
+from main.views import *
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
@@ -23,8 +23,10 @@ urlpatterns = [
     path("", homepage, name="home"),
     path("todo/", todo, name="todo"),
     path("test2/", second, name="second"),
-    path("test3/", third, name="third"),
-    path("bookstore/", bookstore, name="Bookstore")
+    # path("bookstore/", bookstore, name="Bookstore")
+    path("add-todo/", add_todo, name="add-todo"),
+    path("delete-todo/<id>/", delete_todo, name="delete-todo"),
+    path("mark-todo/<id>/", mark_todo, name="mark-todo"),
+    path("close-todo/<id>/", close_todo, name="close-todo"),
 ]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
